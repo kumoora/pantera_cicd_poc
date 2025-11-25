a4614bf10dcfae6ad2153f735e21368e5530041c
kumoora <132431831+kumoora@users.noreply.github.com>
2025-11-25 11:42:21 +0530

    Initial push to staging

--- staging/main.tf
+++ staging/main.tf
@@ -6,21 +6,21 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "<your-bucket-name>"
-    key            = "terraform-staging.tfstate"
-    region         = "<bucket-region>"
-    # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
-    # dynamodb_table = "terraform-state-lock-table"
-    encrypt        = true
-    role_arn       = "arn:aws:iam::<your-aws-account-no>:role/<terraform-s3-backend-access-role>"
-  }
-}
+#   backend "s3" {
+#     bucket         = "<your-bucket-name>"
+#     key            = "terraform-staging.tfstate"
+#     region         = "<bucket-region>"
+#     # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
+#     # dynamodb_table = "terraform-state-lock-table"
+#     encrypt        = true
+#     role_arn       = "arn:aws:iam::<your-aws-account-no>:role/<terraform-s3-backend-access-role>"
+#   }
+# }
 
 provider "snowflake" {
-  username    = "<your_snowflake_username>"
-  account     = "<your_snowflake_account_identifier>"
-  role        = "<your_snowflake_role>"
+  username    = "kumar"
+  account     = "OCMRUDE-LM45005"
+  role        = "ACCOUNTADMIN"
   private_key = var.snowflake_private_key
 }
 
