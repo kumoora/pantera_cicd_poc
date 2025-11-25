2be3034132f13a7f5faebf64667a6a7086c8afb2
kumoora <132431831+kumoora@users.noreply.github.com>
2025-11-25 11:44:23 +0530

    second change

--- staging/main.tf
+++ staging/main.tf
@@ -24,6 +24,7 @@ provider "snowflake" {
   private_key = var.snowflake_private_key
 }
 
+
 module "snowflake_resources" {
   source              = "../modules/snowflake_resources"
   time_travel_in_days = 1
